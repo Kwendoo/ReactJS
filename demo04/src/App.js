45531eebbb78ccced854b2ce2481280a808c17ff
Kwendoo <antoine.lagna@gmail.com>
2021-11-12 11:42:14 +0100

    formaulaire V2

--- demo04/src/App.js
+++ demo04/src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
 import BasiqueForm from './componenets/basique-form/basique-form';
-import ComplexeFormV1 from './componenets/compexe-form/complexe-form-v1';
+// import ComplexeFormV1 from './componenets/compexe-form/complexe-form-v1';
+import ComplexeFormV2 from './componenets/compexe-form/complexe-form-v2';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <BasiqueForm />
 
       <h2> Formulaire complexe</h2>
-      <ComplexeFormV1 />
+      {/* <ComplexeFormV1 /> */}
+      <ComplexeFormV2 />
     </div>
   );
 }
--- demo04/src/componenets/compexe-form/complexe-form-v2.jsx
+++ demo04/src/componenets/compexe-form/complexe-form-v2.jsx
@@ -7,36 +7,96 @@ Contenur du formulaire :
     - Nombre de personne
 */
 
+import { useState } from "react";
+
+const initialData = {
+    email : '',
+    emailError : false,
+    firstName : '',
+    isPresent : false,
+    nbPerson : '0',
+    comment : ''
+}
+
 const ComplexeFormV2 = () => {
 
+    const [inputs, setInputs] = useState(initialData);
 
+    const handleInput = (event, validator) => {
+        const {value, name, checked, type} = event.target;
+
+        if(!validator || validator(value)){
+
+            setInputs(inputs => ({
+                ...inputs,
+                [name] : type === 'checkbox' ? checked : value
+            }));
+
+        }
+
+    }
+
+    const validatorNbPerson = (value) => {
+        const regex = /^[0-9]*$/;
+
+        return regex.test(value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            email : inputs.email,
+            firstName : inputs.firstName,
+            isPresent : inputs.isPresent,
+            nbPerson : inputs.isPresent ? parseInt(inputs.nbPerson) : 0,
+            comment : inputs.comment
+        }
+
+        // cas pratique => Envoie vers un autre composant ou vers une API
+        console.log(data);
+
+        // Reset des valeurs
+        setInputs({...initialData});
+    }
+
+    const {email, emailError, firstName, isPresent, nbPerson, comment} = inputs;
 
     return (
-        <form >
+        <form onSubmit = {handleSubmit}>
             
             <div>
                 <label htmlFor="email">Email</label>
-                <input id='email' type="text" />
+                <input id='email' type="text" name='email'
+                value ={email} onChange={handleInput} 
+                onBlur={(e) => setInputs(i => ({...i, emailError: e.target.value.trim() === ''}))}/>
+                {emailError &&(
+                    <span>L'email est obligatoire !'</span>
+                )}
             </div>
             <div>
                 <label htmlFor="firstName">Prénom</label>
-                <input id='firstName' type="text" />
+                <input id='firstName' type="text" name='firstName'
+                value={firstName} onChange={handleInput}/>
             </div>
             <div>
                 <label htmlFor="isPresent">Présent</label>
-                <input id='isPresent' type="text" />
+                <input id='isPresent' type="checkbox" name='isPresent'
+                checked={isPresent} onChange={handleInput}/>
             </div>
             {isPresent && (
                 <div>
                     <label htmlFor="nbPerson">Nombre de personne</label>
-                    <input id='nbPerson' type="text" />
+                    <input id='nbPerson' type="text" name='nbPerson' 
+                    value={nbPerson} onChange={(e) => handleInput(e, validatorNbPerson)}/>
                 </div>
             )}
             <div>
                 <label htmlFor="comment">Commentaire</label>
-                <textarea id='comment'/>
+                <textarea id='comment' value={comment} name='comment'
+                onChange={handleInput}/>
             </div>
-            <button type='submit'></button>
+            <button type='submit'>Valider</button>
 
         </form>
     );
